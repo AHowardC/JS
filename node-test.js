fcd813edf1a70527d4732036fe9eee79e603f32c
AHowardC <achsvgusaeng@yahoo.com>
2017-09-29 15:43:01 -0400

    anew

---
+++ node-test.js
@@ -0,0 +1,43 @@
+// var counter = 0 
+
+// 	// this is to increment the years 
+// 	for(let i=1701; i <= 1800; i++){
+// 	// this is to increment the months
+// 		for(let j=0; j <= 11; j++){
+// 			var nd = new Date(i, j, 1) 
+// 			if(nd.getDay() == 6 ){
+// 				counter++
+// 			}
+// 		}
+// 	}
+// console.log(counter)
+
+
+var startYear = 1701
+var endYear = 1800
+function monthsThatStartOnSaterday {
+
+	for(year = startYear; year <= 1800; year++)
+		for()
+}
+
+// var year = 1701;
+// var day = 0;
+// var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// var count = 1;
+// while(year <= 1800){
+//     for(let i = 0; i < daysInMonth.length; i++){
+//         if(year % 4 != 0 || year % 100 == 0 && year % 400 != 0){
+//             daysInMonth[1] = 28;
+//         }else{
+//             // console.log(year);
+//             daysInMonth[1] = 29;
+//         }
+//         day += daysInMonth[i];
+//         if (day % 7 == 0){
+//             count++
+//         }
+//     }
+//     year++;
+//     }
+// console.log(count);
