6626901075f804815748229ffb2afa2f51d2cabe
AHowardC <achsvgusaeng@yahoo.com>
2017-10-01 12:06:30 -0400

    new dog

--- practice.js
+++ practice.js
@@ -11,7 +11,14 @@ let cat = {
 let dog = {
 	sound: 'woof'
 }
+let prarieDog = {
+	howl: function(){
+		console.log(this.sound.toUpperCase())
+	}
+}
 Object.setPrototypeOf(cat, animal)
 Object.setPrototypeOf(dog, animal)
+Object.setPrototypeOf(prarieDog, dog)
 cat.talk()
 dog.talk()
+prarieDog.howl()
