b07003956e0ff9cb64a5f26581d66fa9e33f66c7
AHowardC <achsvgusaeng@yahoo.com>
2017-10-01 13:16:41 -0400

    btw

--- practice.js
+++ practice.js
@@ -1,24 +1,64 @@
-function talk() {
-	console.log(this)
-	console.log(this.sound)	
+// function talk() {
+// 	console.log(this)
+// 	console.log(this.sound)	
+// }
+// let animal = {
+// 	talk 
+// }
+// let cat = {
+// 	sound: 'meow'
+// }
+// let dog = {
+// 	sound: 'woof!!'
+// }
+// let prarieDog = {
+// 	howl: function(){
+// 		console.log(this.sound.toUpperCase())
+// 	}
+// }
+// Object.setPrototypeOf(cat, animal)
+// Object.setPrototypeOf(dog, animal)
+// Object.setPrototypeOf(prarieDog, dog)
+// cat.talk()
+// dog.talk()
+// prarieDog.howl()
+
+function Person(saying) {
+	this.saying = saying
 }
-let animal = {
-	talk 
+
+Person.prototype.talk = function(){
+	console.log('I say:', this.saying)
 }
-let cat = {
-	sound: 'meow'
+function new(constructor){
+	var obj = {}
+	Object.setPrototypeOf(obj, constructor.prototype)
+	constructor.apply(obj, [])
 }
-let dog = {
-	sound: 'woof'
-}
-let prarieDog = {
-	howl: function(){
-		console.log(this.sound.toUpperCase())
-	}
-}
-Object.setPrototypeOf(cat, animal)
-Object.setPrototypeOf(dog, animal)
-Object.setPrototypeOf(prarieDog, dog)
-cat.talk()
-dog.talk()
-prarieDog.howl()
+var	crockford = new (Person,'Semicolans! 1one1')
+crockford.talk()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
