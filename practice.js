2c9444d5d0c7032b28dd7aaa4ca855bcc2fcf0a4
AHowardC <achsvgusaeng@yahoo.com>
2017-10-01 11:58:51 -0400

    animal

--- practice.js
+++ practice.js
@@ -1,10 +1,17 @@
-dog = {
-	sound: 'woof',
-	talk: function(){
-		console.log(this.sound)
-	}
+function talk() {
+	console.log(this)
+	console.log(this.sound)	
 }
-
-let button = document.getElementById('myButton')
-
-button.addEventListener('click', dog.talk.bind(dog))
+let animal = {
+	talk 
+}
+let cat = {
+	sound: 'meow'
+}
+let dog = {
+	sound: 'woof'
+}
+Object.setPrototypeOf(cat, animal)
+Object.setPrototypeOf(dog, animal)
+cat.talk()
+dog.talk()
